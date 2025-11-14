b38cb60258ae4d841ea84731b9d49d4fec6a5c1c
DNishadini <dinushikaherath150@gmail.com>
2025-11-14 22:31:54 +0530

    ADD

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Hello from './components/Hello';
+import React from "react";
+import Hello from "./components/Hello";
+import Fruits from "./components/Fruits";
 
 function App() {
-
   return (
     <div className="App">
-      <Hello /> 
-       
+      <Hello />
+      <Fruits />
     </div>
   );
 }
--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-function Hello() {
-  return <h1>Hello from a component!</h1>;
+export default function Fruits() {
+  const fruits = ["Apple", "Mango", "Banana"];
+  return <div>{fruits}</div>;
 }
-
-export default Hello;
