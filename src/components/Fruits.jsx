f305c08bfa8a84012b6744c97535f6adb732272c
DNishadini <dinushikaherath150@gmail.com>
2025-11-14 23:28:16 +0530

    Map() function

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -2,5 +2,13 @@ import React from "react";
 
 export default function Fruits() {
   const fruits = ["Apple", "Mango", "Banana"];
-  return <div>{fruits}</div>;
+  return (
+    <div>
+      <ul>
+        {fruits.map((fruit) => (
+          <li kry={fruit}> {fruit} </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
