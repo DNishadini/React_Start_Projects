d999b7d894fff6ec1ac5073e0e82abaed39a9e14
DNishadini <dinushikaherath150@gmail.com>
2025-11-15 23:40:01 +0530

    Conditionally rendering component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hello from "./components/Hello";
 import Fruits from "./components/Fruits";
+import ConditionalComponent from "./components/ConditionalComponent";
 
 function App() {
   return (
     <div className="App">
       <Hello />
       <Fruits />
+      <ConditionalComponent />
     </div>
   );
 }
