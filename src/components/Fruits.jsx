b79cd4c83e2f8d9c641c8559c69267d20317d6d6
DNishadini <dinushikaherath150@gmail.com>
2025-11-15 20:47:39 +0530

    Arrays of Object 1

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana"];
+  //const fruits = ["Apple", "Mango", "Banana"];
+  const fruits = [
+    { name: "Apple", price: 10, color: "red" },
+    { name: "Mango", price: 7, color: "yellow" },
+    { name: "Orange", price: 5, color: "orange" },
+  ];
   return (
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li kry={fruit}> {fruit} </li>
+          <li key={fruit}>
+            {fruit.name} ${fruit.price} {fruit.color}
+          </li>
         ))}
       </ul>
     </div>
