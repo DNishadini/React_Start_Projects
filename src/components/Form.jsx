3c5bdb7bb41aae51811fd5bba7953c70daf00b6b
DNishadini <dinushikaherath150@gmail.com>
2025-11-17 00:33:08 +0530

    Handling Input Field 3

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,16 +2,12 @@ import { useState } from "react";
 
 export default function Form() {
   const [name, setName] = useState("");
-  function handleChange(e) {
-    console.log(e.target.value);
-  }
+
   return (
     <div>
       <form>
         <input
-          onChange={function demo(e) {
-            return handleChange(e);
-          }}
+          onChange={(e) => setName(e.target.value)}
           type="text"
           value={name}
         />
