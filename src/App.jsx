81dfbf2b232e2c7c2235a04c6abd9c77855bfba2
DNishadini <dinushikaherath150@gmail.com>
2025-11-16 19:43:06 +0530

    State

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Hello from "./components/Hello";
 import Fruits from "./components/Fruits";
 import ConditionalComponent from "./components/ConditionalComponent";
 import { Message } from "./components/Message";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
     </div>
   );
 }
