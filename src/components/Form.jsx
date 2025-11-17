55cb97fb53cdf9f8d2bd45fe1bbc6f8a631f5980
DNishadini <dinushikaherath150@gmail.com>
2025-11-17 11:59:38 +0530

    Handling Form Application

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 export default function Form() {
   const [name, setName] = useState({ firstName: "", lastName: "" });
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+  }
+
   return (
     <div>
       <form>
@@ -18,6 +23,7 @@ export default function Form() {
           onChange={(e) => setName({ ...name, lastName: e.target.value })}
           value={name.lastName}
         />
+        <button onClick={(e) => handleSubmit(e)}>Add</button>
       </form>
     </div>
   );
